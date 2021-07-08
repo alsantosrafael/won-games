9f46f7b45ed79713b28cfa7168339d4257be6ae5
Rafael Almeida <rafael.profeng@gmail.com>
2021-07-07 21:13:18 -0300

    chore: change font and update global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,37 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+	@font-face {
+		font-family: 'Poppins';
+		font-style: normal;
+		font-display: swap;
+		font-weight: 300;
+		src: local('Poppins Light'), local('Poppins-Light'),
+				url('/fonts/poppins-v15-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+	}
+	@font-face {
+		font-family: 'Poppins';
+		font-style: normal;
+		font-display: swap;
+		font-weight: 400;
+		src: local('Poppins Regular'), local('Poppins-Regular'),
+				url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+	}
+	@font-face {
+		font-family: 'Poppins';
+		font-style: normal;
+		font-display: swap;
+		font-weight: 600;
+		src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+				url('/fonts/poppins-v15-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
+	}
+
 	* {
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
 	}
 
 	html {
@@ -14,8 +41,7 @@ const GlobalStyles = createGlobalStyle`
     height: 100%;
   }
 	body {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-
+		font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 	}
 `;
 export default GlobalStyles;
