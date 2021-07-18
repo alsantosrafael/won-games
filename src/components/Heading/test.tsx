e1c08a72895663ce14314717a0531dade694674e
Rafael Almeida <rafael.profeng@gmail.com>
2021-07-18 18:21:13 -0300

    feat: create heading component

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,43 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+import 'jest-styled-components';
+
+import Heading from '.';
+
+describe('<Heading />', () => {
+  it('should render a white heading by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>);
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA',
+    });
+  });
+
+  it('should render a black heading when receiving black in props.color', () => {
+    renderWithTheme(<Heading color="black">Won Games</Heading>);
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#030517',
+    });
+  });
+
+  it('should render a heading with a border to the left', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>);
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      borderLeft: '0.7rem solid #3CD3C1',
+    });
+  });
+
+  it('should render a heading with a border at the bottom', () => {
+    renderWithTheme(<Heading lineBottom={true}>Won Games</Heading>);
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.5rem solid #F231A5',
+      {
+        modifier: '::after',
+      },
+    );
+  });
+});
