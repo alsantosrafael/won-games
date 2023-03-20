6508ced67c367f65d46975371c87a1907c6b8b9b
Rafael Almeida <rafael.profeng@gmail.com>
2023-03-20 13:10:20 -0300

    feat: Create menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,24 +2,69 @@ import * as S from './styles';
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/heroicons-outline/ShoppingCart';
 import { SearchOutline as SearchIcon } from '@styled-icons/evaicons-outline/SearchOutline';
 import { Menu2 as MenuIcon } from '@styled-icons/remix-line/Menu2';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 import Logo from 'components/Logo';
+import { useState } from 'react';
+import Button from 'components/Button';
+import MediaMatch from 'components/MediaMatch';
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon />
-      </S.IconWrapper>
-      <S.IconWrapper>
-        <ShoppingCartIcon />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-);
+
+export type MenuProps = {
+  username?: string
+}
+const Menu = ({ username }: MenuProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <S.Wrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+          </S.IconWrapper>
+      </MediaMatch>
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <MediaMatch greaterThan='medium'>
+        <S.MenuNav>
+          <S.MenuLink>Home</S.MenuLink>
+          <S.MenuLink>Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+        {!username && (
+          <MediaMatch greaterThan='medium'>
+            <Button>Sign in</Button>
+            </MediaMatch>
+        )}
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink>Home</S.MenuLink>
+          <S.MenuLink>Explore</S.MenuLink>
+          {!!username && (
+            <>
+              <S.MenuLink>My account</S.MenuLink>
+              <S.MenuLink>Wishlist</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">Log in now</Button>
+            <span>or</span>
+          <S.CreateAccount href="#" title="Sign up">Sign up</S.CreateAccount>
+          </S.RegisterBox>
+        )}
+      </S.MenuFull>
+    </S.Wrapper>
+  );
+};
 export default Menu;
--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,8 +1,15 @@
-import Menu from '.';
-import { Story, Meta } from '@storybook/react/types-6-0';
+import Menu, { MenuProps } from '.';
+import { Story, Meta } from '@storybook/react/types-7-0';
 export default {
   title: 'Menu',
   component: Menu,
 } as Meta;
 
-export const Default: Story = (args) => <Menu {...args} />;
+export const Default: Story<MenuProps> = (args) => <Menu {...args} />;
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark',
+  },
+};
