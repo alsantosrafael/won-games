ee312da07480c02303dfa923018166a6fd8d5ca4
Rafael Almeida <rafael.profeng@gmail.com>
2023-03-21 13:01:16 -0300

    feat: Expand heading component to build footer

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,9 +1,13 @@
 import * as S from './styles';
+
+export type LineColors = 'primary' | 'secondary'
 export type HeadingProps = {
   children: React.ReactNode;
   color?: 'white' | 'black';
   lineLeft?: boolean;
   lineBottom?: boolean;
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 };
 
 const Heading = ({
@@ -11,8 +15,14 @@ const Heading = ({
   color = 'white',
   lineLeft = false,
   lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+    size={size}>
     {children}
   </S.Wrapper>
 );
