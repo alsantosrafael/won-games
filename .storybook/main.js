e2ae3bc0449914c080eb7306e061261708ebb73b
Rafael Almeida <rafael.profeng@gmail.com>
2023-03-24 08:26:23 -0300

    build(deps): Update storybook and peer dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/components/**/stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: (config) => {
+  stories: ['../src/**/*.mdx', '../src/components/**/stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  webpackFinal: config => {
     config.resolve.modules.push(`${process.cwd()}/src`);
     return config;
   },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
