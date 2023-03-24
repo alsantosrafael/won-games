d10aff966ff5691ed17f7a712b22176e44186e8b
Rafael Almeida <rafael.profeng@gmail.com>
2023-03-24 09:32:30 -0300

    fix: Remove mistaken type from story

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,4 +1,4 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
+import { Story, Meta } from '@storybook/react/types-7-0'
 import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 import Button from '.'
 export default {
@@ -12,7 +12,7 @@ export default {
       type: ''
     }
   }
-} as unknown as Meta
+} as Meta
 export const Default: Story = (args) => <Button {...args} />
 Default.args = {
   children: 'Buy now'
